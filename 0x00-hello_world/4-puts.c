cb1cd98cb0fa71f6b5911a89007214c95b170fd9
johnngure <johnwngure@gmail.com>
2022-09-08 12:14:50 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 
 /**
 
-* main - print the string in the puts function
+* main - print the string in the put function
 
 *
 
@@ -16,14 +16,13 @@
 
 */
 
-
-
 int main(void)
 
 {
 
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
 }
+
