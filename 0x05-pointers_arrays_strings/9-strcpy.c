8f24686f30fc534ff61490b7afdb30fa1b3b6ade
johnngure <johnwngure@gmail.com>
2022-09-19 12:35:15 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
-* _strcpy - Copy paste string 
+* _strcpy - Copy paste string
 * @dest: destination
 * @src: source
+*
 * Return: destination
 */
 
-char *_strcpy(char *dest, char *src) 
+char *_strcpy(char *dest, char *src)
 {
 	int inc = 0;
 
@@ -19,5 +20,5 @@ char *_strcpy(char *dest, char *src)
 	*(dest + inc) = '\0';
 
 
-	return (dest);
+	return(dest);
 }
