7d07895cde069aa0a0c1d264366639957cd27fc2
johnngure <johnwngure@gmail.com>
2022-10-27 12:23:32 -0700

    reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,11 +8,11 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *p;
-    listint_t *n;
+	listint_t *p;
+	listint_t *n;
 
-    p = NULL;
-    n = NULL;
+	p = NULL;
+	n = NULL;
 
 	while (*head != NULL)
 	{
