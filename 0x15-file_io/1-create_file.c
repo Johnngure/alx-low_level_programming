becb9de8ca47bcfca62e0533bfc411394099cb13
johnngure <johnwngure@gmail.com>
2022-11-02 11:21:14 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,52 @@
 #include "main.h"
+#include <stddef.h>
+
+/**
+ * _strlen - counts string length
+ * @str: string to be used
+ *
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
 
 /**
  * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * @filename: name of the file
+ * @text_content: content of the file to be created
+
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 on success, -1 otherwise
  */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int file, wrote;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
-
-	return (1);
+	if (text_content != NULL)
+	{
+		wrote = write(file, text_content, _strlen(text_content));
+		if (wrote == -1)
+		{
+			close(file);
+			return (-1);
+		}
+		close(file);
+		return (1);
+	}
+	else
+	{
+		close(file);
+		return (1);
+	}
 }
